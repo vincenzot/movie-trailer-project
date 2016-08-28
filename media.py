4672ee279d0566214c271757ec937f20dafbe524
vincenzot <enzo.t.1@hotmail.it>
2016-08-28 14:50:08 +0200

    Update media.py

--- media.py
+++ media.py
@@ -9,10 +9,34 @@ class Movie():
     picture:
         It's the film image url
         ex. "http://nsa37.casimages.com/img/2015/09/25/150925121855869372.jpg"
+        
 
+    year:
+        The movie release year
     
+    lenght:
+        Movie lenght expressed in minutes
+        
+    trailer_url:
+        A trailer url from YouTube of the movie
     
+    director:
+        Movie director
+    
+    genre:
+        Movie genre, ex. drama, comic etc..
+    
+    actors:
+        Main actors of the movie
+    
+    plot:
+        Movie description
+    
+    mpaa_icon:
+        Movie classification icon url
+        
     """
+    
     def __init__(self,
                  title,
                  picture,
